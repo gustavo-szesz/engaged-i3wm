8501065a67f87350d16d077b3e4cf6835d64f831
Gustavo Szesz <szeszgustavo@gmail.com>
2026-04-29 20:04:53 -0300

    feat: Add Polybar launch script

---
+++ config/polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Polybar launch script
+# Kill existing polybar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch polybar
+polybar main &
+
+echo "Polybar launched!"
